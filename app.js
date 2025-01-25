877ed3fcc0732f1dcf7fd13fcee8cb6153e67e64
arleydemelquisedeclisbet <fervercar9@gmail.com>
2025-01-24 22:10:37 -0500

    Crea función para actualizar lista de amigos

--- app.js
+++ app.js
@@ -3,13 +3,25 @@
 const amigos = [];
 
 const inputNombreAmigo = document.getElementById('amigo');
+const listaAmigos = document.getElementById('listaAmigos');
 
 // Función para agregar amigos
 const agregarAmigo = () => {
   if (inputNombreAmigo.value) {
     amigos.push(inputNombreAmigo.value);
     inputNombreAmigo.value = '';
+    actualizarListaAmigos();
     return;
   }
   alert('Por favor, inserte un nombre');
+}
+
+// Función para actualizar la lista de amigos
+const actualizarListaAmigos = () => {
+  listaAmigos.innerHTML = '';
+  amigos.forEach(amigo => {
+    const li = document.createElement('li');
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+  });
 }
